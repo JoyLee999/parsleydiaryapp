aefc1546bf680d421e11dbbdef23de50a5e3d8aa
leselsey <leselsey@outlook.com>
2021-04-28 15:49:19 +0800

    add alert message

--- src/helpers/emailValidator.js
+++ src/helpers/emailValidator.js
@@ -1,6 +1,6 @@
 export function emailValidator(email) {
   const re = /\S+@\S+\.\S+/
-  if (!email) return "Email can't be empty."
-  if (!re.test(email)) return 'Ooops! We need a valid email address.'
+  if (!email) return "Can't empty the email field."
+  if (!re.test(email)) return 'Sorry! We already have this email.'
   return ''
 }
