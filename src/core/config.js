50c5eb0cff6a15e5ccfbd29795f05b46a32ef1a4
leselsey <leselsey@outlook.com>
2021-04-28 15:39:25 +0800

    connect firebase

--- src/core/config.js
+++ src/core/config.js
@@ -1,11 +1,10 @@
 // Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyBDsdN3XWM5D2-c_ti5j44GrhMnBHNmSF0',
-  authDomain: 'react-native-market-2861b.firebaseapp.com',
-  databaseURL: 'https://react-native-market-2861b.firebaseio.com',
-  projectId: 'react-native-market-2861b',
-  storageBucket: 'react-native-market-2861b.appspot.com',
-  messagingSenderId: '878215484396',
-  appId: '1:878215484396:web:c36074bccb0b5693b0a6c9',
-  measurementId: 'G-317PLP59P2',
+  apiKey: "AIzaSyC_DaFt4BbRmedZcuwIi18H5hoH4IcRluk",
+  authDomain: "parsley-diary.firebaseapp.com",
+  projectId: "parsley-diary",
+  storageBucket: "parsley-diary.appspot.com",
+  messagingSenderId: "295323618501",
+  appId: "1:295323618501:web:fd60b961957d2ee05174ac",
+  measurementId: "G-D87X9PXTTQ"
 }
